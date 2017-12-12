fc2d98aafddcfee9c8a54c6303d42d5de74322b7
Daniel <varvara_sd@yahoo.co.uk>
2017-12-12 13:50:16 +0000

    add testing of retrieval of single pet owner

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -4,12 +4,21 @@ import 'truffle/Assert.sol';
 import 'truffle/DeployedAddresses.sol';
 import '../contracts/Adoption.sol';
 
-contract TestAdoption{
+contract TestAdoption {
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
-    function userCanAdoptPet(){
+    // Testing the adopt() function
+    function userCanAdoptPet() {
         uint returnedId = adoption.adopt(8);
         uint expected = 8;
         Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded");
     }
+    // Testing retrieval of a single pet's owner
+    function testGetAdopterAddressByPetId() {
+        // Expected owner is this contract
+        address expected = this;
+        address adopter = adoption.adopters(8);
+        Assert.equal(adopter, expected, "Owner of pet ID 8 should be recoreded");
+    }
+
 }
