7e08d183d5fcefa52e183f0bf3bf55275a44fe2e
Daniel <varvara_sd@yahoo.co.uk>
2017-12-12 15:17:05 +0000

    indent edit

--- src/js/app.js
+++ src/js/app.js
@@ -75,24 +75,24 @@ App = {
     var petId = parseInt($(event.target).data('id'));
 	var adoptionInstance;
 
-web3.eth.getAccounts(function(error, accounts) {
-  if (error) {
-    console.log(error);
-  }
+	web3.eth.getAccounts(function(error, accounts) {
+		if (error) {
+			console.log(error);
+		}
 
-  var account = accounts[0];
+		var account = accounts[0];
 
-  App.contracts.Adoption.deployed().then(function(instance) {
-    adoptionInstance = instance;
+		App.contracts.Adoption.deployed().then(function(instance) {
+			adoptionInstance = instance;
 
-    // Execute adopt as a transaction by sending account
-    return adoptionInstance.adopt(petId, {from: account});
-  }).then(function(result) {
-    return App.markAdopted();
-  }).catch(function(err) {
-    console.log(err.message);
-  });
-});
+			// Execute adopt as a transaction by sending account
+			return adoptionInstance.adopt(petId, {from: account});
+			}).then(function(result) {
+				return App.markAdopted();
+			}).catch(function(err) {
+				console.log(err.message);
+		});
+	});
   }
 
 };
