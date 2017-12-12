5e1875d1f45f614c2be401f2d5bb175ad7f8baa7
Daniel <varvara_sd@yahoo.co.uk>
2017-12-12 13:59:18 +0000

    add testing of retrieval of all pet owners

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -8,17 +8,31 @@ contract TestAdoption {
     Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
     // Testing the adopt() function
-    function userCanAdoptPet() {
+    function testUserCanAdoptPet() {
         uint returnedId = adoption.adopt(8);
+
         uint expected = 8;
-        Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded");
+
+        Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
     }
-    // Testing retrieval of a single pet's owner
+        // Testing retrieval of a single pet's owner
     function testGetAdopterAddressByPetId() {
         // Expected owner is this contract
         address expected = this;
+
         address adopter = adoption.adopters(8);
-        Assert.equal(adopter, expected, "Owner of pet ID 8 should be recoreded");
+
+        Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+    }
+    // Testing retrieval of all pet owners
+    function testGetAdopterAddressByPetIdInArray() {
+        // Expected owner is this contract
+        address expected = this;
+
+        // Store adopters in memory rather than contract's storage
+        address[16] memory adopters = adoption.getAdopters();
+
+        Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
     }
 
 }
