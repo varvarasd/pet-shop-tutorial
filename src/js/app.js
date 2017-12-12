1c5b239dd2bb3b1304ce5d9b74dcd37f333ed0a7
Daniel <varvara_sd@yahoo.co.uk>
2017-12-12 14:48:24 +0000

    Creating a UI to interact with the smart contract: 1) instantiating web3 -  2) instantiiating the contract

--- src/js/app.js
+++ src/js/app.js
@@ -24,18 +24,27 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
+	// is there an injected web3 instance?
+	if(typeof web3 !== 'undefined'){
+		App.web3Provider = web3.currentProvider;
+	} else {
+		// if no injected web3 instance is detected, fallback to the TestRPC
+		App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+	}
+	web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
+    $.getJSON('Adoption.json', function(data){
+		// Get the necessary contract artifact file and instantiate it with truffle-contract
+		var AdoptionArtifact = data;
+		App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+		// Set the provider for our contract
+		App.contracts.Adoption.SetProvider(App.web3Provider);
+		// use our contract to retrieve and mark the adopted pets
+		return App.markAdopted();
+	})
     return App.bindEvents();
   },
 
